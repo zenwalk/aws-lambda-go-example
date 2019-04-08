e4cf48191738aa4e4de6e23aa5a4a65b52fa1a1a
chenn <cyruson@gmail.com>
2019-04-08 22:32:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		Body:       request.headers,
 	}, nil
 }
 
